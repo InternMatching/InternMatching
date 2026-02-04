10a89d243fbc28840d5f89611d7648453394c515
Oyunbat <oyunbat9958@gmail.com>
2026-02-04 13:35:14 +0800

    layout changed

--- lib/apollo-client.ts
+++ lib/apollo-client.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
-
+// || 'http://localhost:4000/graphql'
 const httpLink = new HttpLink({
-  uri:  process.env.NEXT_PUBLIC_GRAPHQL_URL || 'http://localhost:4000/graphql',
+  uri:  process.env.NEXT_PUBLIC_GRAPHQL_URL ,
 });
 export const client = new ApolloClient({
   link: httpLink,
